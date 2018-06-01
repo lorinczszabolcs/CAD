111eac79bccb762fef63e0f2650fdc0c585dedc4
datami97 <tamasdarvas@yahoo.com>
2018-06-01 16:09:59 +0300

    valami

--- FirstOrderAlgebraicTrigonometric/FirstOrderAlgebraicTrigonometricCompositeSurfaces3.cpp
+++ FirstOrderAlgebraicTrigonometric/FirstOrderAlgebraicTrigonometricCompositeSurfaces3.cpp
@@ -2,12 +2,7 @@
 #include "Core/Materials.h"
 
 using namespace cagd;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-=======
->>>>>>> parent of 615955c... javitas
 /*
 >>>>>>> parent of 615955c... javitas
 FirstOrderAlgebraicTrigonometricCompositeSurface3::PatchAttributes::PatchAttributes()
